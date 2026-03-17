fab3c88dd86b17b0a07e2ff1a0be463f30341b40
leon_lenovo <leofloresv10@gmail.com>
2026-03-17 17:33:44 -0600

    Feat: Begining

---
+++ main.js
@@ -0,0 +1,17 @@
+//Contenido de los servicios
+
+const servicios = [
+    { nombre: "Paginas web", precio: "1000", descripccion: "Creacion de paginas web", imagen: "/jiji"},
+    { nombre: "Soporte tecnico", precio: "300", descripccion: "Reparacion de equipos", imagen: "/jiji"},
+    { nombre: "Auditorias", precio: "4000", descripccion: "Revision de tus servicios", imagen: "/jiji"},
+    { nombre: "Automatizacion de pagos", precio: "3000", descripccion: "Ayuda para gestionar pagos", imagen: "/jiji"},
+    { nombre: "Marketing", precio: "500", descripccion: "Ayuda en la gestion y creacion de redes sociales", imagen: "/jiji"},
+    { nombre: "Aplicaciones moviles", precio: "6000", descripccion: "Te desarrollamos una aplicacion movil", imagen: "/jiji"},
+    { nombre: "Redes", precio: "2000", descripccion: "Para implementar una buena comunicacion en tu empresa", imagen: "/jiji"},
+    { nombre: "Aministrar de Bases de datos", precio: "10000", descripccion: "Creamos, gestionamos y estandarizamos un modelo de base de datos adecuado para tu empresa", imagen: "/jiji"},
+    { nombre: "Subnetear redes", precio: "5000", descripccion: "Ayuda para la administracion de redes", imagen: "/jiji"},
+    { nombre: "Hosting", precio: "5000", descripccion: "Proporcionamos el servicio para que puedaas hostear tu pagina web", imagen: "/jiji"},
+]
+
+//Refernenciar cons base de html
+const contenedor  = document.getElementById('contenedor-servicios');
